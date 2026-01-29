08d00176f83fc8a0c6e1bb03727f553a903fd437
Emma Raissa <nenguemraissa@yahoo.fr>
2026-01-29 10:10:58 -0500

    Demarrage de tout le projet (backend et next.js)

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ import sse from './middlewares/sse.js'
 import passport from 'passport'
 import session from 'express-session'
 import memorystore from 'memorystore'
-const app=express()
+import next from "next";
 
+// Defini si nous sommes en production ou en developpement
+const dev = process.env.NODE_ENV !== "production";
+
+const PORT = process.env.PORT ;
+const nextApp = next({ dev, dir: "./emy_app" });
+const handle = nextApp.getRequestHandler();
+
+await nextApp.prepare();
+
+const app=express()
 const MemoryStore=memorystore(session)
 
 app.use(helmet())
 app.use(cors())
 app.use(compression())
-app.use(json())
+app.use(express.json())
 app.use(session({
     name:process.env.npm_package_name,
     cookie:{maxAge:3600000},
@@ -27,7 +37,15 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(sse())
-app.use(express.static('app'))
 
-app.listen(process.env.PORT);
-console.log('http://localhost:' + process.env.PORT);
+// API pour tester le backend
+app.get("/api/health", (req, res) => res.json({ ok: true }));
+
+
+// Specifier que toutes les requêtes restantes, Next.js s’en occupe
+// Doit toujours etre place apres toutes les requetes HTTP
+app.all(/.*/, (req, res) => handle(req, res));
+
+app.listen(PORT, () => {
+  console.log(`Serveur unique: http://localhost:${PORT} (dev=${dev})`);
+});
