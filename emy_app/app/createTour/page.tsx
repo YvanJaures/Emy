ee280adf3094596351f23c71483a61b102f6e943
Mathieu Larocque <2706544@collegelacite.ca>
2026-03-12 15:52:09 -0400

    Validation : nb équipes >= 0, et cohérence avec équipes déjà créées ---> fait

--- emy_app/app/createTour/page.tsx
+++ emy_app/app/createTour/page.tsx
@@ -31,6 +31,7 @@ export default function CreateTournament() {
   const [endDate, setEndDate] = useState("");
   const [fees, setFees] = useState("");
   const [status, setStatus] = useState("1");
+  const [teams, setTeams] = useState("");
   //const [idAdmin, setIdAdmin] = useState("");
   //const [idCommunity, setIdCommunity] = useState("");
 
@@ -65,6 +66,11 @@ export default function CreateTournament() {
       return;
     }
 
+    if (Number(teams) < 0) {
+    setError("Le nombre d'équipes doit être supérieur ou égal à 0.");
+    return;
+    }
+
     setLoading(true);
 
     try {
@@ -73,6 +79,7 @@ export default function CreateTournament() {
         start_date: startDate,
         end_date: endDate,
         status: status ? Number(status) : 1,
+        teams: Number(teams),
         avatar: avatarFile ? avatarFile.name : "",
         id_admin: Number(id_admin),
         id_community: Number(id_community),
@@ -225,6 +232,16 @@ export default function CreateTournament() {
                 value={fees}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setFees(e.target.value)
+                }
+              />
+              <InputText
+                label="Frais d'inscription"
+                containerClassName="flex flex-row flew-wrap justify-center items-center"
+                type="text"
+                required
+                value={fees}
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFees(e.target.value)
                 }
               />
             </div>
