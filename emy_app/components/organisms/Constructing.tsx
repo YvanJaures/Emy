d593db983d9a3c5e5a95111aa6a3f76b20dce2c9
yvan <yvanjauresnzali@gmail.com>
2026-03-16 21:42:19 -0400

    finission sur la page de communaute pop up

---
+++ emy_app/components/organisms/Constructing.tsx
@@ -0,0 +1,10 @@
+export default function Constructing(){
+    return(
+        <div className="bg-[#FBC300] w-full h-full object-contain flex justify-center items-center">
+            <p className="text-4xl animate-pulse text-white font-bold absolute w-73 h-37 p-3 text-center
+                 -translate-y-7.5 translate-x-0.5 max-sm:-translate-y-6.8 max-sm:w-44.5 max-sm:h-23 max-sm:text-xl max-sm:translate-x-0.05 ">Fonctionnalitée en cours de développement<sub className="animate-pulse">...</sub></p>
+            <img src="/assets/emy_travaux.png" alt="image de construction" 
+                className="w-150 drop-shadow-xl"/>
+        </div>
+    )
+}
