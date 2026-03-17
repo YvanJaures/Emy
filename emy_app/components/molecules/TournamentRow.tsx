03439b598a63eed0e6a3a12b82f71fc32181183a
Mathieu Larocque <2706544@collegelacite.ca>
2026-03-17 18:04:51 -0400

    Merge du Formulaire qui n'avait pas fonctionner... donc je l'ai ai ajouté manuellement

--- emy_app/components/molecules/TournamentRow.tsx
+++ emy_app/components/molecules/TournamentRow.tsx
@@ -7,10 +7,16 @@ import type { TournamentDTO } from "@/hooks/Type_TournamentDTO";
 export default function TournamentRow({
   tournament,
   onDelete,
+  onToggleStatus,
   isDeleting,
 }: {
   tournament: TournamentDTO;
   onDelete: (id: number) => void;
+  onToggleStatus: (
+    id_tour: number,
+    id_community: number,
+    status: number
+  ) => void;
   isDeleting?: boolean;
 }) {
   const title = tournament.location?.trim()
@@ -23,8 +29,12 @@ export default function TournamentRow({
     onDelete(tournament.id_tour);
   };
 
+  const handleInscription = () => {
+    alert(`Inscription au tournoi ${tournament.id_tour}`);
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-2 rounded-2xl bg-white px-5 py-3 shadow sm:grid-cols-3 sm:items-center">
+    <div className="grid grid-cols-1 gap-2 rounded-2xl bg-white px-5 py-3 shadow sm:grid-cols-5 sm:items-center">
       <p className="text-xs text-black/80">{title}</p>
 
       <div className="flex justify-start sm:justify-center">
@@ -36,7 +46,42 @@ export default function TournamentRow({
         </AppLink>
       </div>
 
-      <div className="flex justify-start sm:justify-end">
+       <div className="flex justify-start sm:justify-center">
+        <button
+          type="button"
+          disabled={tournament.status === 0}
+          onClick={handleInscription}
+          className={`text-xs underline-offset-4 ${
+            tournament.status === 1
+              ? "text-green-600 hover:underline"
+              : "text-gray-400 cursor-not-allowed"
+          }`}
+        >
+          {tournament.status === 1
+            ? "Inscription"
+            : "Inscriptions fermées"}
+        </button>
+      </div>
+
+      <div className="flex justify-start sm:justify-center">
+        <button
+        type="button"
+        onClick={() =>
+        onToggleStatus(
+        tournament.id_tour,
+        tournament.id_community!,
+        tournament.status
+      )
+      }
+        className="text-xs text-blue-500 hover:text-blue-800 hover:underline hover:cursor-pointer transition-colors duration-200"
+        >
+        {tournament.status === 1
+          ? "Fermer inscriptions"
+          : "Ouvrir inscriptions"}
+        </button>
+      </div>
+
+      <div className="flex justify-start sm:justify-center">
         <button
           type="button"
           onClick={handleDeleteClick}
