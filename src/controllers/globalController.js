2ae4b64e691bc51620e3c0f215f7fc3dad3eb816
yvan <yvanjauresnzali@gmail.com>
2026-04-24 12:46:18 -0400

    console.log("user : "+user)

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -49,6 +49,7 @@ export const stream=async (request, response) => {
  */
 export const getUser = async (request, response) => {
   const user = request.user;
+  console.log("user : "+user)
   if (!user) {
     response.status(200).json(null);;
     return;
