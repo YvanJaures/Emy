25e318a1b7b75c2f865b5acb646468e651efc9a0
yvan <yvanjauresnzali@gmail.com>
2026-04-27 10:35:13 -0400

    get to axios.get

--- src/keepAlive.js
+++ src/keepAlive.js
@@ -1,11 +1,11 @@
 import { schedule } from "node-cron";
-import { get } from "axios";
+import { axios } from "axios";
 
 const SERVER_URL = process.env.RENDER_URL;
 
 schedule("*/10 * * * *", async () => {
   try {
-    const res = await get(SERVER_URL);
+    const res = await axios.get(SERVER_URL);
     console.log(`[Keep-alive] Ping OK — ${new Date().toISOString()} — status: ${res.status}`);
   } catch (err) {
     console.error(`[Keep-alive] Ping échoué — ${err.message}`);
